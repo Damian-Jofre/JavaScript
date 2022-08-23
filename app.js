a9506a77bc6342cb9ba2c3286558705367916cf9
DamianJ <dajar80@gmail.com>
2022-08-23 14:56:06 -0300

    modifiqué ejemplo 2

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-/*  Ejemplo 1: Pedir un número que le sume otro y muestre el resultado
+// Ejemplo 1: Pedir un número mediante prompt y sumarle otro número en cada repetición, realizando una salida por cada resultado.
 
-let numero = prompt("Ingrese un número")
+/* let numero = prompt("Ingrese un número")
 
 do {
     numero ++;
     console.log(numero);
 } while (numero<10); */
 
-/* Ejemplo 2: Pedir un texto mediante prompt concatenar un valor en cada repeticion, realizando una salida por cada resultado
+// Ejemplo 2: Pedir un texto mediante prompt, concatenar un valor en cada repeticion, realizando una salida por cada resultado, hasta que se ingresa "ESC"
 
-let texto = prompt("Ingrese texto, para finalizar ingrese ESC")
+/* let texto = prompt("Ingrese texto, para finalizar ingrese ESC");
+let resultado = " ";
 
 while(texto != "ESC"){
-    console.log(texto)
+    resultado += " ";
+    resultado += texto;
+    console.log(resultado);
     texto = prompt("Ingrese texto, para finalizar ingrese ESC");
-} */
+}  */
 
 
 /* Ejemplo 3: Pedir un número por prompt, repetir la salida del mensaje "Hola" la cantidad de veces ingresada.
