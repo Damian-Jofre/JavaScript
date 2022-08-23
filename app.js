74b014c40c023947cbc6a6188e8ea9d3ceac95ef
DamianJ <dajar80@gmail.com>
2022-08-23 18:10:58 -0300

    subo primer desafío

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Ejemplo 1: Pedir un número mediante prompt y sumarle otro número en cada repetición, realizando una salida por cada resultado. En este caso puse un maximo de 10, para que no se haga al infinito.
 
-/* let numero = prompt("Ingrese un número")
+let numero = prompt("Ingrese un número")
 
 do {
     numero ++;
     console.log(numero);
-} while (numero<10); */
+} while (numero<10);
 
 
 // Ejemplo 2: Pedir un texto mediante prompt, concatenar un valor en cada repeticion, realizando una salida por cada resultado, hasta que se ingresa "ESC"
@@ -21,7 +21,7 @@ while(texto != "ESC"){
 }  */
 
 
-// Ejemplo 3: Pedir un número por prompt, repetir la salida del mensaje "Hola" la cantidad de veces ingresada. (En este ejemplo no puedo hacer que salga por consola bien, por eso use alert)
+// Ejemplo 3: Pedir un número por prompt, repetir la salida del mensaje "Hola" la cantidad de veces ingresada. (En este ejemplo no puede hacer que salga bien por consola, por eso usé alert)
 
 /* let numero = Number(prompt("Ingrese un número"));
 const hola = "HOLA!!!!!";
